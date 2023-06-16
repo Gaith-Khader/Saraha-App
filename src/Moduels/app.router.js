6c5b1771cfb42fe38e29a7520dcaff654f1e9e53
Gaith-Khader <gaith9khader@gmail.com>
2023-06-16 17:04:57 +0300

    'edit'

--- src/Moduels/app.router.js
+++ src/Moduels/app.router.js
@@ -6,12 +6,12 @@ import UserRouter from './User/User.router.js'
 
 const initApp=(app,express)=>{
     app.use(express.json());
-    app.use('/',(req,res)=>{
-        res.send('hello')
-    });
     app.use('/auth', AuthRouter);
     app.use('/message', MessageRouter);
     app.use('/users',auth, UserRouter);
+    app.use('/',(req,res)=>{
+        res.send('hello')
+    });
     app.use('/*',(req,res)=>{
         return res.json({message:'page not found'})
     })
